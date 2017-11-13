c690b1c00ac37a31b391809415cd334814fba02c
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-11-12 20:36:10 -0800

    rage

--- server.js
+++ server.js
@@ -4,19 +4,18 @@ const app = express();
 const PORT = process.env.PORT;
 const cors = require('cors');
 const superagent = require('superagent');
+const token = $process.env.GITHUB_TOKEN
 
 app.use(express.static('./public'));
 app.use(cors());
 
 app.get('/github/*', (request, response) => {
   console.log('Routing a gh api request for ', request.params[0])
+  console.log(`authstring: token ${token}`)
   superagent(`https://api.GitHub.com/${request.params[0]}`)
-    .set('Authorization',`token $process.env.GITHUB_TOKEN`)
+    .set('Authorization',`token ${token}`)
     .then(
-      repos => {
-        console.log(repos.text)
-        response.send(repos.text)
-      },
+      repos => response.send(repos.text),
       err => response.send(`https://api.GitHub.com/${request.params[0]} Request Fail: ${err}`)
     )
 })
